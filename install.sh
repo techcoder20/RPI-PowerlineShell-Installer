bc4ea9a02bb327c2f84c7a50b2184cdd7838d6fe
techcoder <techcoder20@gmail.com>
2020-12-17 05:27:13 -0600

    Added Comments

--- install.sh
+++ install.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-sudo apt install python3 python-pip fonts-powerline fonts-fantasque-sans
+#Installing dependencies
+sudo apt install python3 python-pip fonts-powerline fonts-fantasque-sans 
+
+#Installing powerline shell with pip3
 pip3 install powerline-shell
+
+#Creating back directory
 mkdir ~/RPI-PowerlineShell-Installer/.backup
+
+#Creating backup of lxterminal configs
 sudo cp -r ~/.config/lxterminal ~/RPI-PowerlineShell-Installer/.backup
+
+#Copying powerline shell configs
 sudo cp -r ~/RPI-PowerlineShell-Installer/lxterminal ~/.config
 
+#Removing powerline in ~/.bashrc if already there
 sed -i '/function _update_ps1() {/,/fi/{d}' ~/.bashrc
 
+#Adding powerling to ~/.bashrc
 echo '''function _update_ps1() {
     PS1=$(powerline-shell $?)
 }
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+#Removing powerline shell with pip
 pip3 uninstall -y powerline-shell
+
+#Removing lxterminal configs
 sudo rm -r ~/.config/lxterminal
+
+#Copying back up lxterminal configs
 sudo cp -r ~/RPI-PowerlineShell-Installer/.backup/lxterminal ~/.config
 
+#Removing powerline shell from ~/.bashrc
 sed -i '/function _update_ps1() {/,/fi/{d}' ~/.bashrc
