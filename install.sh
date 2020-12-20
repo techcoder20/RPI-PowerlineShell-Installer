5b61818f7179c9834f6f1c9ad288ac0fb8c52fdd
Botspot <54716352+Botspot@users.noreply.github.com>
2020-12-19 21:42:09 -0600

    change to size 12 font for lxterminal

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo apt install python3 python-pip fonts-powerline fonts-fantasque-sans  || err
 sudo pip3 install powerline-shell || error "Failed to install powerline shell with pip3"
 
 #Copying powerline shell configs
-sed -i '/fontname/c\fontname=Fantasque Sans Mono Bold 11' ~/.config/lxterminal/lxterminal.conf
+sed -i '/fontname/c\fontname=Fantasque Sans Mono Bold 12' ~/.config/lxterminal/lxterminal.conf
 sed -i '/FontName/c\FontName=Fantasque Sans Mono 12' ~/.config/xfce4/terminal/terminalrc
 
 #Removing powerline in ~/.bashrc if already there
