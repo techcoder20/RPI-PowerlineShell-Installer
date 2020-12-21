275091a8e5edf8852b6c49ab1a8872ebe4a5376e
RPICoder <68472305+techcoder20@users.noreply.github.com>
2020-12-20 22:52:37 -0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,8 +3,8 @@
 #Removing powerline shell with pip
 sudo pip3 uninstall -y powerline-shell
 
-sed -i '/FontName/d' ~/.config/xfce4/terminal/terminalrc
-sed -i '/fontname/d' ~/.config/lxterminal/lxterminal.conf
+sed -i '/FontName/c\FontName=Fantasque Sans Mono Bold 11' ~/.config/xfce4/terminal/terminalrc
+sed -i '/fontname/c\fontname=Fantasque Sans Mono Bold 11' ~/.config/lxterminal/lxterminal.conf
 
 #Removing powerline shell from ~/.bashrc
 sed -i '/function _update_ps1() {/,/fi/{d}' ~/.bashrc
