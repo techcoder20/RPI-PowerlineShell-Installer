7cb359fe705484785c37ed3aaaa869315bd7c52e
RPICoder <68472305+techcoder20@users.noreply.github.com>
2020-12-20 22:17:42 -0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,8 +3,8 @@
 #Removing powerline shell with pip
 sudo pip3 uninstall -y powerline-shell
 
-#Removing lxterminal configs
-sudo rm -r ~/.config/lxterminal
+sed -i '/FontName/d' ~/.config/xfce4/terminal/terminalrc
+sed -i '/fontname/d' ~/.config/lxterminal/lxterminal.conf
 
 #Removing powerline shell from ~/.bashrc
 sed -i '/function _update_ps1() {/,/fi/{d}' ~/.bashrc
